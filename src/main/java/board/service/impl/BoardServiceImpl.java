54eee934794a83305ea11fcfad296f02b4c92c3a
kjc0411 <jaechang3288@gmail.com>
2021-06-18 00:51:54 +0900

    create BoardService, BoardServiceImpl

---
+++ src/main/java/board/service/impl/BoardServiceImpl.java
@@ -0,0 +1,10 @@
+package board.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import board.service.BoardService;
+
+@Service("boardService")
+public class BoardServiceImpl implements BoardService {
+
+}
